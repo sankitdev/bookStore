edcbc7a6089d898628b5bc0a66c51f60ffaf1a0a
Ankit Singh <ankitsingh635101@gmail.com>
2024-12-21 12:05:48 +0530

    Populated Book Data and refactored the code

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,30 +1,28 @@
+import useFetchBook from "../hooks/useFetchBook"
+import TableHead from "./TableHead"
 const Table: React.FC = () => {
- 
+  const bookData = useFetchBook()
+  if(!bookData) return
+  else console.log(bookData)
   return (
 <table className="table">
   <thead>
-    <tr>
-      <th scope="col">Book Id</th>
-      <th scope="col">Book Name</th>
-      <th scope="col">Book Desc</th>
-      <th scope="col">Book Author</th>
-      <th scope="col">No of Page</th>
-      <th scope="col">Book Category</th>
-      <th scope="col">Book Price</th>
-      <th scope="col">Released Year</th>
-    </tr>
+    <TableHead/>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>The Great Gatsby</td>
-      <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, et magnam. Molestiae, molestias consequuntur quidem illo autem sint omnis maxime quis quod vitae quasi necessitatibus temporibus tenetur. Voluptatum, debitis asperiores.</td>
-      <td>F. Scott Fitzgerald</td>
-      <td>180</td>
-      <td>Fiction</td>
-      <td>10.899</td>
-      <td>1925</td>
+    {bookData.map((books)=>{
+      const {bookId, bookName, bookDesc, bookAuthor,noOfPages,bookCategory,bookPrice, releasedYear} = books
+      return <tr>
+      <th scope="row">{bookId}</th>
+      <td>{bookName}</td>
+      <td>{bookDesc}</td>
+      <td>{bookAuthor}</td>
+      <td>{noOfPages}</td>
+      <td>{bookCategory}</td>
+      <td>{bookPrice}</td>
+      <td>{releasedYear}</td>
     </tr>
+    })}
   </tbody>
 </table>
   )
