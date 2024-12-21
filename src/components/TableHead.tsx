1fff4902ef62640763656a1c961b7785fa07ab3e
Ankit Singh <ankitsingh635101@gmail.com>
2024-12-21 12:03:55 +0530

    Create Table Head Component for table headers

---
+++ src/components/TableHead.tsx
@@ -0,0 +1,16 @@
+const TableHead = () => {
+  return (
+    <tr>
+      <th scope="col">Book Id</th>
+      <th scope="col">Book Name</th>
+      <th scope="col">Book Desc</th>
+      <th scope="col">Book Author</th>
+      <th scope="col">No of Page</th>
+      <th scope="col">Book Category</th>
+      <th scope="col">Book Price</th>
+      <th scope="col">Released Year</th>
+    </tr>
+  );
+};
+
+export default TableHead;
