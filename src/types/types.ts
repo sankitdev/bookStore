c70a8bc3cad1767736fb111bc6d311e4f2bc7966
Ankit Singh <ankitsingh635101@gmail.com>
2024-12-22 11:05:35 +0530

    Add Book type definition to types.ts

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export type Book = {
+  bookId: number;
+  bookName: string;
+  bookDes: string;
+  bookAuthor: string;
+  noOfPages: number;
+  bookCategory: string;
+  bookPrice: number;
+  releasedYear: number;
+};
