f8ee4baa04d48b9fa1e4058652ff224c2830f817
Ankit Singh <ankitsingh635101@gmail.com>
2024-12-22 11:05:06 +0530

    Enhance Table component with action buttons and dynamic rendering of book data

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,35 +1,48 @@
-import useFetchBook from "../hooks/useFetchBook"
-import Pagination from "./Pagination"
-import TableHead from "./TableHead"
-const Table: React.FC = () => {
-  const bookData = useFetchBook()
-  if(!bookData) return
-  else console.log(bookData)
-  return (
-<>
-<table className="table">
-  <thead>
-    <TableHead/>
-  </thead>
-  <tbody>
-    {bookData.map((books)=>{
-      const {bookId, bookName, bookDesc, bookAuthor,noOfPages,bookCategory,bookPrice, releasedYear} = books
-      return <tr>
-      <th scope="row">{bookId}</th>
-      <td>{bookName}</td>
-      <td>{bookDesc}</td>
-      <td>{bookAuthor}</td>
-      <td>{noOfPages}</td>
-      <td>{bookCategory}</td>
-      <td>{bookPrice}</td>
-      <td>{releasedYear}</td>
-    </tr>
-    })}
-  </tbody>
-</table>
-<Pagination/>
-</>
-  )
-}
+import useFetchBook from "../hooks/useFetchBook";
+import Button from "./Button";
+import Pagination from "./Pagination";
+import TableHead from "./TableHead";
+import { BOOK_ACTION_BUTTONS, handleFunction } from "../constant";
+import { Book } from "../types/types";
+const Table = () => {
+  const bookData = useFetchBook();
+  if (!bookData) return;
 
-export default Table
+  function handleAction(action: string) {
+    if (handleFunction[action]) {
+      handleFunction[action]();
+    } else {
+      alert("Function not implemented");
+    }
+  }
+  return (
+    <>
+      {BOOK_ACTION_BUTTONS.map((btns) => (
+        <Button text={btns.label} action={() => handleAction(btns.action)} />
+      ))}
+      <table className="table">
+        <thead>
+          <TableHead />
+        </thead>
+        <tbody>
+          {bookData.map((books) => {
+            const { bookId } = books;
+            return (
+              <tr key={bookId}>
+                <th scope="row">{bookId}</th>
+                {Object.keys(books)
+                  .filter((key) => key !== "bookId")
+                  .map((key) => (
+                    <td key={bookId}>{books[key as keyof Book]}</td>
+                  ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination />
+    </>
+  );
+};
+
+export default Table;
