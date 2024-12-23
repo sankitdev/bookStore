141d99838a9653c2f7f5da8767998aad702ef54f
Ankit Singh <ankitsingh635101@gmail.com>
2024-12-23 10:50:42 +0530

    Refactor Table component to utilize useTableState for action handling and improve key assignment in table rows

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,31 +1,32 @@
 import useFetchBook from "../hooks/useFetchBook";
 import Button from "./Button";
 import TableHead from "./TableHead";
-import { BOOK_ACTION_BUTTONS, handleFunction } from "../constant";
+import { BOOK_ACTION_BUTTONS } from "../constant";
 import { Book } from "../types/types";
+import useTableState from "../hooks/useTableState";
 const Table = () => {
+  const { index, handleAction } = useTableState();
   const bookData = useFetchBook();
   if (!bookData) return;
 
-  function handleAction(action: string) {
-    if (handleFunction[action]) {
-      handleFunction[action]();
-    } else {
-      alert("Function not implemented");
-    }
-  }
   return (
     <>
       {BOOK_ACTION_BUTTONS.map((btns) => {
         const { label, action } = btns;
-        return <Button text={label} action={() => handleAction(action)} />;
+        return (
+          <Button
+            key={action}
+            text={label}
+            action={() => handleAction(action)}
+          />
+        );
       })}
       <table className="table">
         <thead>
           <TableHead />
         </thead>
         <tbody>
-          {bookData.map((books) => {
+          {bookData.slice(index, index + 5).map((books) => {
             const { bookId } = books;
             return (
               <tr key={bookId}>
@@ -33,7 +34,7 @@ const Table = () => {
                 {Object.keys(books)
                   .filter((key) => key !== "bookId")
                   .map((key) => (
-                    <td key={bookId}>{books[key as keyof Book]}</td>
+                    <td key={`${bookId}-${key}`}>{books[key as keyof Book]}</td>
                   ))}
               </tr>
             );
