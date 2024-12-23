77398b363f26585240f15c45d87e6c7c9e8b129b
Ankit Singh <ankitsingh635101@gmail.com>
2024-12-23 17:20:31 +0530

    Refactor Modal component to support dynamic form fields and submission handling

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,14 +1,25 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-
+import Form from "react-bootstrap/Form";
+import { ModalProps } from "../types/types";
+import { useState } from "react";
+import { modalConfig } from "../config/modalConfig";
 function Model({
   isModelOpen,
   setIsModelOpen,
-}: {
-  isModelOpen: boolean;
-  setIsModelOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}) {
-  const handleClose = () => setIsModelOpen(false);
+  modalType,
+  onSubmit,
+}: ModalProps) {
+  const [formData, setFormData] = useState<Record<string, string>>({});
+  const config = modalConfig[modalType];
+  const handleClose = () => {
+    setIsModelOpen(false);
+    setFormData({});
+  };
+  const handleSubmit = () => {
+    onSubmit(formData);
+    handleClose();
+  };
   return (
     <>
       <Modal
@@ -18,17 +29,31 @@ function Model({
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>{config.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Do not even try to press
-          escape key.
+          {config.fields.map((field) => (
+            <Form.Control
+              key={field.name}
+              type="text"
+              placeholder={field.placeholder}
+              className="mb-2"
+              onChange={(e) =>
+                setFormData((prev) => ({
+                  ...prev,
+                  [field.name]: e.target.value,
+                }))
+              }
+            />
+          ))}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Understood</Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            Update
+          </Button>
         </Modal.Footer>
       </Modal>
     </>
