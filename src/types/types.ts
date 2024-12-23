2588e7d7e88f24ead1ad9394ec6d5fe3d1443424
Ankit Singh <ankitsingh635101@gmail.com>
2024-12-23 17:20:53 +0530

    Add modal type handling to types for improved modal component integration

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,5 @@
+import { MODAL_TYPES } from "../config/modalConfig";
+
 export type Book = {
   bookId: number;
   bookName: string;
@@ -15,6 +17,14 @@ export type StateSetters = {
   ) => void;
   setIsModelOpen: (isOpen: boolean) => void;
   setBooks: React.Dispatch<React.SetStateAction<Book[]>>;
+  setModelType: React.Dispatch<React.SetStateAction<string>>;
   setOriginalBooks: React.Dispatch<React.SetStateAction<Book[]>>;
   originalBooks: Book[];
+};
+
+export type ModalProps = {
+  isModelOpen: boolean;
+  setIsModelOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  modalType: keyof typeof MODAL_TYPES;
+  onSubmit: (data: Record<string, string>) => void;
 };
