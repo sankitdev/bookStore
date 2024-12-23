85499ca9777d0827baa7c5adada76164eb70bd82
Ankit Singh <ankitsingh635101@gmail.com>
2024-12-23 12:53:11 +0530

    Enhance useTableState hook to manage filter, sort, and modal visibility states

--- src/hooks/useTableState.ts
+++ src/hooks/useTableState.ts
@@ -1,18 +1,28 @@
 import { useState } from "react";
-import { bookActions } from "../utils/bookActions";
+import { bookActions } from "../utils/bookActions.ts";
 
 function useTableState() {
-  const [index, setIndex] = useState(0);
-
+  const [index, setIndex] = useState<number | null>(0);
+  const [filter, setFilter] = useState<string | null>(null);
+  const [sort, setSort] = useState<string | null>(null);
+  const [isModelOpen, setIsModelOpen] = useState<boolean>(false);
   function handleAction(action: string) {
     if (bookActions[action]) {
-      bookActions[action]({setIndex});
+      bookActions[action]({ setIndex, setFilter, setSort, setIsModelOpen });
     } else {
       alert("Function not implemented");
     }
   }
 
-  return { index, setIndex, handleAction };
+  return {
+    index,
+    setIndex,
+    handleAction,
+    isModelOpen,
+    setIsModelOpen,
+    filter,
+    sort,
+  };
 }
 
 export default useTableState;
